e09bee61fb367baaa19abcc39acc1320e248addb
Code Sandbox <hello@codesandbox.io>
2022-08-13 03:40:26 +0000

    Deployment from Codesandbox.

---
+++ exercicio1-javascript.js
@@ -0,0 +1,33 @@
+document.getElementById("hw1").innerHTML = `
+<h2> Hello Word 1 </h2>
+`;
+
+document.getElementById("hw2").innerHTML = `
+Hello Word 2 !! 
+`;
+
+document.getElementById("hw3").innerHTML = `
+<h4> Hello Word 3 !!! </h4>
+`;
+
+document.getElementById("hw5").innerHTML = `
+<h1> Hello Word 5 !!! </h1>
+<p style="color: red;"> Hello Word 6</p>
+`;
+
+document.getElementById("hw7").innerHTML = `
+<span style="font-size: x-large;">
+<span style="color: gray;">
+Hello Word 7 
+</span>
+</span>
+`;
+
+document.getElementById("hw8").style.fontSize = "25px";
+document.getElementById("hw8").style.color = "purple";
+document.getElementById("hw8").style.backgroundColor = "black";
+
+document.getElementById("hw9").style.fontFamily = "verdana";
+document.getElementById("hw9").style.color = "green";
+
+document.getElementById("hw10").style = "border:2px solid Tomato";
